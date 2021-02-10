c3558d96604b2027b75d44e453cbd51939be0e08
Elfa Andika <elfaandhika@gmail.com>
2021-02-10 16:57:25 +0700

    Initial Commit

---
+++ CongklakIndonesiaGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CongklakIndonesiaGame
+//
+//  Created by Elfa Andika on 10/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
